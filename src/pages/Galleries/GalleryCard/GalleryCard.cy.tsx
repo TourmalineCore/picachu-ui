a9a579609241b0a7a97579a67fc4b9d5c009a726
Ekaterina <echekina@gmail.com>
2023-03-23 11:40:15 +0500

    test(galleries): #861mh296p: add test to check that onNameChange callback is called when we focus out in focused name field

--- src/pages/Galleries/GalleryCard/GalleryCard.cy.tsx
+++ src/pages/Galleries/GalleryCard/GalleryCard.cy.tsx
@@ -47,5 +47,28 @@ describe(`GalleryCard`, () => {
       cy.get(`@onNameChange`)
         .should(`have.been.calledOnceWith`, `new gallery123`);
     });
+
+    it(`SHOULD apply changes to name WHEN focus out in focused name`, () => {
+      const onNameChangeSpy = cy.spy().as(`onNameChange`);
+
+      cy.mount(
+        <GalleryCard
+          imagePath="#"
+          imageAlt=""
+          photosCount={0}
+          name="new gallery"
+          newlyCreated
+          onNameChange={onNameChangeSpy}
+        />,
+      );
+
+      cy
+        .getByData(`gallery-name-input`)
+        .type(`!!!`)
+        .blur();
+
+      cy.get(`@onNameChange`)
+        .should(`have.been.calledOnceWith`, `new gallery!!!`);
+    });
   });
 });
