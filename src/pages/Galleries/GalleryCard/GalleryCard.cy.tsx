d38843924614406fd13fa9fddd9d83f3f60531c8
Ekaterina <echekina@gmail.com>
2023-03-23 11:00:45 +0500

    test(galleries): #861mh296p: add test to check that name is focused after creation

--- src/pages/Galleries/GalleryCard/GalleryCard.cy.tsx
+++ src/pages/Galleries/GalleryCard/GalleryCard.cy.tsx
@@ -2,19 +2,25 @@ import GalleryCard from './GalleryCard';
 import '../../../../cypress/support/commands';
 
 describe(`GalleryCard`, () => {
-  beforeEach(() => {
-    // const onLogin = cy.spy().as(`onLogin`);
+  // beforeEach(() => {
+  //   // const onLogin = cy.spy().as(`onLogin`);
+
+  // });
+
+  it(`newly created gallery card SHOULD have name field filled and focused`, () => {
     cy.mount(
       <GalleryCard
         imagePath="#"
         imageAlt=""
         photosCount={0}
+        name="new gallery"
+        newlyCreated
       />,
     );
-  });
 
-  it(`empty card SHOULD be mounted`, () => {
-    // cy.getByData(`password-input`)
-    //   .should(`be.empty`);
+    cy
+      .getByData(`gallery-name-input`)
+      .should(`have.value`, `new gallery`)
+      .focused();
   });
 });
