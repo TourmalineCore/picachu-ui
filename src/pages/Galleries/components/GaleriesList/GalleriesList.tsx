abe9b92a55d7bf089421cfc4bf532c6233a98c69
Yasnovsky <andrey5.1997@mail.ru>
2023-03-24 14:11:56 +0500

    test(galleries): add test case for newly created card to check that name is focused

--- src/pages/Galleries/components/GaleriesList/GalleriesList.tsx
+++ src/pages/Galleries/components/GaleriesList/GalleriesList.tsx
@@ -1,19 +1,22 @@
 import GalleryCard from "../GalleryCard/GalleryCard";
 
 function GalleriesList({
+  newlyCreatedGalleryId,
   galleries,
 }: {
+  newlyCreatedGalleryId: number;
   galleries: any[];
 }) {
   return (galleries.length === 0
     ? <span data-cy="no-galleries">Create a gallery to get started</span>
     : (
       galleries.map(({
+        id,
         name,
       }) => (
         <GalleryCard
           name={name}
-          newlyCreated={false}
+          newlyCreated={newlyCreatedGalleryId === id}
           onNameApply={(newName) => {
           }}
           photosCount={0}
