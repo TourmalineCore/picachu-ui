5e2b521c58393fd39f0c09dee0dd6eeeefcc9248
Yasnovsky <andrey5.1997@mail.ru>
2023-03-24 13:53:37 +0500

    feat(galleries): render GalleryCard

--- src/pages/Galleries/components/GaleriesList/GalleriesList.tsx
+++ src/pages/Galleries/components/GaleriesList/GalleriesList.tsx
@@ -1,10 +1,22 @@
+import GalleryCard from "../GalleryCard/GalleryCard";
+
 function GalleriesList({
   galleries,
 }: {
   galleries: any[];
 }) {
   return (galleries.length === 0
-    && <span data-cy="no-galleries">Create a gallery to get started</span>
+    ? <span data-cy="no-galleries">Create a gallery to get started</span>
+    : (
+      <GalleryCard
+        name={galleries[0].name}
+        newlyCreated={false}
+        onNameApply={(newName) => {
+        }}
+        photosCount={0}
+        photos={[]}
+      />
+    )
   );
 }
 
