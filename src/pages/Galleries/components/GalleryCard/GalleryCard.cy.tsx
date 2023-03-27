d1e0d97d7aad441a8fc2569612bd108d3994c0df
Ekaterina <echekina@gmail.com>
2023-03-27 16:25:36 +0500

    test(galleries): #861mjdjb1: refactoring - extract gallery card testing functions under namespaces

--- src/pages/Galleries/components/GalleryCard/GalleryCard.cy.tsx
+++ src/pages/Galleries/components/GalleryCard/GalleryCard.cy.tsx
@@ -3,6 +3,16 @@ import '../../../../../cypress/support/commands';
 import GalleryCard from './GalleryCard';
 
 describe(`GalleryCard`, () => {
+  describe(`name default rendering`, defaultNameRenderingTests);
+
+  describe(`name editing`, nameEditingTests);
+
+  describe(`photo preview`, photoPreviewTests);
+
+  describe(`delete gallery`, deleteGalleryTests);
+});
+
+function defaultNameRenderingTests() {
   it(`newly created gallery card SHOULD have name field filled and focused`, () => {
     mountComponent({
       id: 1,
@@ -14,170 +24,173 @@ describe(`GalleryCard`, () => {
       .should(`have.value`, `new gallery`)
       .focused();
   });
+}
 
-  describe(`name editing`, () => {
-    it(`SHOULD apply changes to name WHEN pressing Enter in focused name`, () => {
-      const onNameApplySpy = cy.spy().as(`onNameApply`);
+function nameEditingTests() {
+  it(`SHOULD apply changes to name WHEN pressing Enter in focused name`, () => {
+    const onNameApplySpy = cy.spy().as(`onNameApply`);
 
-      mountComponent({
-        id: 1,
-        newlyCreated: true,
-        onNameApply: onNameApplySpy,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .type(`123`)
-        .should(`have.value`, `new gallery123`)
-        .type(`{enter}`);
-
-      checkOnApplyCalledOnce({
-        expectedName: `new gallery123`,
-      });
+    mountComponent({
+      id: 1,
+      newlyCreated: true,
+      onNameApply: onNameApplySpy,
     });
 
-    it(`SHOULD apply changes to name WHEN focus out in focused name`, () => {
-      const onNameApplySpy = cy.spy().as(`onNameApply`);
+    cy
+      .getByData(`gallery-name-input`)
+      .type(`123`)
+      .should(`have.value`, `new gallery123`)
+      .type(`{enter}`);
 
-      mountComponent({
-        id: 1,
-        newlyCreated: true,
-        onNameApply: onNameApplySpy,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .type(`!!!`)
-        .blur();
-
-      checkOnApplyCalledOnce({
-        expectedName: `new gallery!!!`,
-      });
-    });
-
-    it(`SHOULD discard changes to name and apply it with original name WHEN pressing Escape in focused name`, () => {
-      const onNameApplySpy = cy.spy().as(`onNameApply`);
-
-      mountComponent({
-        id: 1,
-        newlyCreated: true,
-        onNameApply: onNameApplySpy,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .type(`777`)
-        .type(`{esc}`);
-
-      checkOnApplyCalledOnce({
-        expectedName: `new gallery`,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .should(`have.value`, `new gallery`);
-    });
-
-    it(`SHOULD discard changes to name and apply it with original name WHEN pressing Enter in focused empty name`, () => {
-      const onNameApplySpy = cy.spy().as(`onNameApply`);
-
-      mountComponent({
-        id: 1,
-        newlyCreated: true,
-        onNameApply: onNameApplySpy,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .clear()
-        .type(`{enter}`);
-
-      checkOnApplyCalledOnce({
-        expectedName: `new gallery`,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .should(`have.value`, `new gallery`);
-    });
-
-    it(`SHOULD discard changes to name and apply it with original name WHEN focus out in focused empty name`, () => {
-      const onNameApplySpy = cy.spy().as(`onNameApply`);
-
-      mountComponent({
-        id: 1,
-        newlyCreated: true,
-        onNameApply: onNameApplySpy,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .clear()
-        .blur();
-
-      checkOnApplyCalledOnce({
-        expectedName: `new gallery`,
-      });
-      cy
-        .getByData(`gallery-name-input`)
-        .should(`have.value`, `new gallery`);
-    });
-
-    it(`SHOULD have the possibility to edit name WHEN it is not a newly created one`, () => {
-      const onNameApplySpy = cy.spy().as(`onNameApply`);
-
-      mountComponent({
-        id: 1,
-        name: `my super`,
-        newlyCreated: false,
-        onNameApply: onNameApplySpy,
-      });
-
-      cy
-        .getByData(`gallery-name-input`)
-        .click()
-        .type(` duper`)
-        .blur();
-
-      checkOnApplyCalledOnce({
-        expectedName: `my super duper`,
-      });
+    checkOnApplyCalledOnce({
+      expectedName: `new gallery123`,
     });
   });
 
-  describe(`photo preview`, () => {
-    it(`SHOULD show dummy picture WHEN no image is available`, () => {
-      mountComponent({
-        id: 1,
-        newlyCreated: true,
-        photos: [],
-      });
+  it(`SHOULD apply changes to name WHEN focus out in focused name`, () => {
+    const onNameApplySpy = cy.spy().as(`onNameApply`);
 
-      cy
-        .getByData(`gallery-photo-preview`)
-        .should(`have.attr`, `alt`)
-        .and(`equal`, `No photos have been added to new gallery yet`);
+    mountComponent({
+      id: 1,
+      newlyCreated: true,
+      onNameApply: onNameApplySpy,
+    });
+
+    cy
+      .getByData(`gallery-name-input`)
+      .type(`!!!`)
+      .blur();
+
+    checkOnApplyCalledOnce({
+      expectedName: `new gallery!!!`,
     });
   });
 
-  describe(`delete gallery`, () => {
-    it(`SHOULD delete a gallery WHEN this gallery's Delete button is clicked`, () => {
-      const onDeleteSpy = cy.spy().as(`onDelete`);
+  it(`SHOULD discard changes to name and apply it with original name WHEN pressing Escape in focused name`, () => {
+    const onNameApplySpy = cy.spy().as(`onNameApply`);
 
-      mountComponent({
-        id: 1,
-        newlyCreated: false,
-        photos: [],
-        onDelete: onDeleteSpy,
-      });
-      cy
-        .getByData(`delete-gallery`).click();
+    mountComponent({
+      id: 1,
+      newlyCreated: true,
+      onNameApply: onNameApplySpy,
+    });
 
-      cy.get(`@onDelete`)
-        .should(`have.been.calledOnce`);
+    // typeTextInName(`777`)
+    //   .type(`{esc}`);
+
+    cy
+      .getByData(`gallery-name-input`)
+      .type(`777`)
+      .type(`{esc}`);
+
+    checkOnApplyCalledOnce({
+      expectedName: `new gallery`,
+    });
+
+    cy
+      .getByData(`gallery-name-input`)
+      .should(`have.value`, `new gallery`);
+  });
+
+  it(`SHOULD discard changes to name and apply it with original name WHEN pressing Enter in focused empty name`, () => {
+    const onNameApplySpy = cy.spy().as(`onNameApply`);
+
+    mountComponent({
+      id: 1,
+      newlyCreated: true,
+      onNameApply: onNameApplySpy,
+    });
+
+    cy
+      .getByData(`gallery-name-input`)
+      .clear()
+      .type(`{enter}`);
+
+    checkOnApplyCalledOnce({
+      expectedName: `new gallery`,
+    });
+
+    cy
+      .getByData(`gallery-name-input`)
+      .should(`have.value`, `new gallery`);
+  });
+
+  it(`SHOULD discard changes to name and apply it with original name WHEN focus out in focused empty name`, () => {
+    const onNameApplySpy = cy.spy().as(`onNameApply`);
+
+    mountComponent({
+      id: 1,
+      newlyCreated: true,
+      onNameApply: onNameApplySpy,
+    });
+
+    cy
+      .getByData(`gallery-name-input`)
+      .clear()
+      .blur();
+
+    checkOnApplyCalledOnce({
+      expectedName: `new gallery`,
+    });
+    cy
+      .getByData(`gallery-name-input`)
+      .should(`have.value`, `new gallery`);
+  });
+
+  it(`SHOULD have the possibility to edit name WHEN it is not a newly created one`, () => {
+    const onNameApplySpy = cy.spy().as(`onNameApply`);
+
+    mountComponent({
+      id: 1,
+      name: `my super`,
+      newlyCreated: false,
+      onNameApply: onNameApplySpy,
+    });
+
+    cy
+      .getByData(`gallery-name-input`)
+      .click()
+      .type(` duper`)
+      .blur();
+
+    checkOnApplyCalledOnce({
+      expectedName: `my super duper`,
     });
   });
-});
+}
+
+function photoPreviewTests() {
+  it(`SHOULD show dummy picture WHEN no image is available`, () => {
+    mountComponent({
+      id: 1,
+      newlyCreated: true,
+      photos: [],
+    });
+
+    cy
+      .getByData(`gallery-photo-preview`)
+      .should(`have.attr`, `alt`)
+      .and(`equal`, `No photos have been added to new gallery yet`);
+  });
+}
+
+function deleteGalleryTests() {
+  it(`SHOULD delete a gallery WHEN this gallery's Delete button is clicked`, () => {
+    const onDeleteSpy = cy.spy().as(`onDelete`);
+
+    mountComponent({
+      id: 1,
+      newlyCreated: false,
+      photos: [],
+      onDelete: onDeleteSpy,
+    });
+    cy
+      .getByData(`delete-gallery`).click();
+
+    cy.get(`@onDelete`)
+      .should(`have.been.calledOnce`);
+  });
+}
 
 function checkOnApplyCalledOnce(
   {
