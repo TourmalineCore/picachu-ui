8aaa45e69fe99df1a3aeb402ae692602dbc2a532
Ekaterina <echekina@gmail.com>
2023-03-29 10:18:36 +0500

    test(galleries): #861mk3q4j: update GalleriesPage tests

--- src/pages/Galleries/GalleriesPage.cy.tsx
+++ src/pages/Galleries/GalleriesPage.cy.tsx
@@ -1,78 +1,56 @@
 import '../../../cypress/support/commands';
 import GalleriesPage from './GalleriesPage';
 
-describe(`GalleriesPage`, () => {
-  it(`SHOULD render call to action message WHEN there are no galleries`, () => {
-    mountComponent();
+// Found bugs/nuances ;)
+// explains partially why we need this different aliases https://stackoverflow.com/questions/66765452/intercept-the-same-api-call-multiple-times-in-cypress
 
-    cy.intercept(`GET`, `/galleries`, []);
+describe(`GalleriesPage`, () => {
+  it(`SHOULD render no galleries message WHEN there are no galleries`, () => {
+    cy.intercept(`GET`, `/api/galleries`, []);
+
+    mountComponent();
 
     cy
       .getByData(`no-galleries`)
       .contains(`Create a gallery to get started`);
   });
 
-  it(`SHOULD call backend and then render the new gallery card WHEN click on create new gallery`, () => {
+  it(`SHOULD call backend to create WHEN click on create new gallery and not changing the name`, () => {
+    cy.intercept(`GET`, `/api/galleries`, [{
+      id: 1,
+      name: `First Gallery`,
+    }]);
+
     mountComponent();
 
-    cy.intercept(`GET`, `/galleries`, []);
+    // we should see the existing gallery, it is not newly created, thus, not focused
+    cy.getByData(`gallery-name-input`)
+      .should(`have.value`, `First Gallery`)
+      .should(`not.be.focused`);
 
-    cy
-      .getByData(`no-galleries`)
-      .contains(`Create a gallery to get started`);
+    const newGalleryId = 2;
 
-    const newGalleryId = 1;
-
-    cy.intercept(`POST`, `/galleries`, {
-      statusCode: 200,
+    cy.intercept(`POST`, `/api/galleries`, {
       body: newGalleryId,
     });
 
-    // we expect that after this click we called the endpoint to create a new gallery and get the ID back
+    // ask to create a new gallery
     cy
-      .getByData(`no-galleries-create-gallery-button`)
+      .getByData(`add-button`)
       .click();
+
+    const onRenameBackendCallSpy = cy.spy().as(`onRenameBackendCallSpy`);
+
+    cy.intercept(`PUT`, `/api/galleries/${newGalleryId}/update-name`, onRenameBackendCallSpy);
 
     // the gallery name has to be focused since we just created it
+    // and if press enter, rename shouldn't be called since the name is the same
     cy
       .getByData(`gallery-name-input`)
-      .focused();
-  });
-
-  it(`SHOULD call backend and then render the new gallery card WHEN click on create new gallery`, () => {
-    mountComponent();
-
-    cy.intercept(`GET`, `/galleries`, []);
-
-    cy
-      .getByData(`no-galleries`)
-      .contains(`Create a gallery to get started`);
-
-    const newGalleryId = 1;
-
-    cy.intercept(`POST`, `/galleries`, {
-      statusCode: 200,
-      body: newGalleryId,
-    });
-
-    cy
-      .getByData(`no-galleries-create-gallery-button`)
-      .click();
-
-    cy
-      .getByData(`gallery-name-input`)
-      .focused();
-
-    cy.intercept(`PUT`, `/galleries/${newGalleryId}/update-name`, (req) => {
-      expect(req.body.name, `Tourmaline Core Gallery`);
-    });
-
-    // we should be able to rename it and save
-    cy
-      .getByData(`gallery-name-input`)
-      .clear()
-      .type(`Tourmaline Core Gallery`)
+      .focused()
       .type(`{enter}`);
+
+    cy.get(`@onRenameBackendCallSpy`).should(`not.have.been.called`);
   });
 });
 
