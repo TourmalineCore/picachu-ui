fa48f5d6fcfea208cd10f4167d68450205599aa5
Ekaterina <echekina@gmail.com>
2023-04-02 16:59:42 +0500

    test(galleries): #861mk5p2q: add test to check deleted gallery's name

--- src/pages/Galleries/components/RestoreDeletedGallery/RestoreDeletedGallery.cy.tsx
+++ src/pages/Galleries/components/RestoreDeletedGallery/RestoreDeletedGallery.cy.tsx
@@ -3,9 +3,7 @@ import RestoreDeletedGallery from './RestoreDeletedGallery';
 
 describe(`RestoreDeletedGallery`, () => {
   it(`SHOULD call restore callback WHEN restore button is clicked`, () => {
-    const onRestoreGallerySpy = cy.spy().as(`onRestoreGallery`);
-
-    mountComponent({ onRestoreGallery: onRestoreGallerySpy });
+    mountComponent();
 
     cy
       .getByData(`restore-gallery-button`).click();
@@ -13,16 +11,21 @@ describe(`RestoreDeletedGallery`, () => {
     cy.get(`@onRestoreGallery`)
       .should(`have.been.calledOnce`);
   });
+
+  it(`SHOULD show corresponding gallery's name WHEN deleting a gallery`, () => {
+    mountComponent();
+
+    cy.contains(`city`);
+  });
 });
 
-function mountComponent({
-}: {
-}) {
+function mountComponent() {
   const onRestoreGallerySpy = cy.spy().as(`onRestoreGallery`);
 
   cy.mount(
     <RestoreDeletedGallery
       onRestoreGallery={onRestoreGallerySpy}
+      galleryName="city"
     />,
   );
 }
