8f077bd516fc2eb3eb17ae3d1814b5e82f85e2bc
Yasnovsky <andrey5.1997@mail.ru>
2023-04-12 13:04:28 +0500

    chore: fix script for check lint and cypress folder

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -2,13 +2,14 @@ import "./commands";
 
 /// <reference types="cypress" />
 
-import { MountReturn, mount } from "cypress/react18";
 import "../../src/styles/index.scss";
 
+import { ReactNode } from "react";
 import { MemoryRouter, MemoryRouterProps } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { MountReturn, mount } from "cypress/react18";
 import { MountOptions } from 'cypress/react';
-import { ReactNode } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,20 +23,15 @@ const queryClient = new QueryClient({
 declare global {
   namespace Cypress {
     interface Chainable {
-      /**
-       * Mounts a React node
-       * @param component React Node to mount
-       * @param options Additional options to pass into mount
-       */
       mount(
-        component: React.ReactNode,
+        component: ReactNode,
         options?: MountOptions & { routerProps?: MemoryRouterProps }
       ): Cypress.Chainable<MountReturn>;
     }
   }
 }
 
-Cypress.Commands.add(`mount`, (component: ReactNode, options: MountOptions & { routerProps?: MemoryRouterProps } = {}) => {
+Cypress.Commands.add(`mount`, (component, options: MountOptions & { routerProps?: MemoryRouterProps } = {}) => {
   const {
     routerProps = {
       initialEntries: [`/`],
