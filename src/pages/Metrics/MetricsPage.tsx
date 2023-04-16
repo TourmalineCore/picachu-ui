7fe68f27d05415af0c5b52621aee79dbdd372f29
ShabanooovArt <ShabanoooovArt@yandex.ru>
2023-04-17 02:17:43 +0500

    feat(metric page): #85zrv2dp4: add loaders

--- src/pages/Metrics/MetricsPage.tsx
+++ src/pages/Metrics/MetricsPage.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-nested-ternary */
+import { useEffect, useState } from 'react';
 import metricImage from '../../assets/images/metric-image.png';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 import CircleProgressBar from '../../components/CircleProgressBar/CircleProgressBar';
@@ -6,11 +8,24 @@ import {
   associationsArray, colorsArray, emotionsArray, objectsArray,
 } from './MetricsTestValues.data';
 import MetricsSimilarCard from './components/MetricsSimilarCard/MetricsSimilarCard';
+import MetricSimilarLoader from '../../assets/images/similar-loader-image.svg';
 
 function MetricsPage() {
   const TotalUniqueness = 52;
   const Colors = 20;
   const Other = 15;
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const delay = 1;
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => setIsLoading(false), delay * 1000);
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, []);
+
   return (
     <div className="metrics-page">
       <Breadcrumbs />
@@ -27,31 +42,55 @@ function MetricsPage() {
           <div className="metrics-page__uniqueness">
             <h1 className="metrics-page__title">Uniqueness</h1>
             <div className="metrics-page__uniqueness-bars">
-              <CircleProgressBar
-                percentage={TotalUniqueness}
-                circleWidth={72}
-                strokeWidth={7}
-                radius={32}
-                isPercentageShown
-              />
+              {
+                isLoading ? (
+                  <div className="metrics-page__image-loader-container">
+                    <span className="metrics-page__image-loader" />
+                    <span className="metrics-page__image-loader-text">counting</span>
+                  </div>
+                ) : (
+                  <CircleProgressBar
+                    percentage={TotalUniqueness}
+                    circleWidth={72}
+                    strokeWidth={7}
+                    radius={32}
+                    isPercentageShown
+                  />
+                )
+              }
+
               <div className="metrics-page__stroke-bars">
                 <div className="metrics-page__box">
                   <p className="metrics-page__subtitle">Colors</p>
 
-                  <div className="metrics-page__bar">
-                    <div className="metrics-page__bar-background">
-                      <div
-                        className="metrics-page__bar-progress"
-                        style={{
-                          width: `${Colors}%`,
-                        }}
-                      />
+                  {isLoading ? (
+                    <div
+                      className="metrics-page__loader-container"
+                    >
+                      <span className="metrics-page__loader--purple" />
+                      <span
+                        className="metrics-page__image-loader-text"
+                      >
+                        {Colors}
+                        % completed
+                      </span>
                     </div>
-                    <h1 className="metrics-page__bar-percentage-text">
-                      {Colors}
-                      <span>%</span>
-                    </h1>
-                  </div>
+                  ) : (
+                    <div className="metrics-page__bar">
+                      <div className="metrics-page__bar-background">
+                        <div
+                          className="metrics-page__bar-progress"
+                          style={{
+                            width: `${Colors}%`,
+                          }}
+                        />
+                      </div>
+                      <h1 className="metrics-page__bar-percentage-text">
+                        {Colors}
+                        <span>%</span>
+                      </h1>
+                    </div>
+                  )}
 
                 </div>
                 <div className="metrics-page__box">
@@ -61,20 +100,36 @@ function MetricsPage() {
                     associations
                   </p>
 
-                  <div className="metrics-page__bar">
-                    <div className="metrics-page__bar-background">
+                  {
+                    isLoading ? (
                       <div
-                        className="metrics-page__bar-progress"
-                        style={{
-                          width: `${Other}%`,
-                        }}
-                      />
-                    </div>
-                    <h1 className="metrics-page__bar-percentage-text">
-                      {Other}
-                      <span>%</span>
-                    </h1>
-                  </div>
+                        className="metrics-page__loader-container"
+                      >
+                        <span className="metrics-page__loader--purple" />
+                        <span
+                          className="metrics-page__image-loader-text"
+                        >
+                          {Other}
+                          % completed
+                        </span>
+                      </div>
+                    ) : (
+                      <div className="metrics-page__bar">
+                        <div className="metrics-page__bar-background">
+                          <div
+                            className="metrics-page__bar-progress"
+                            style={{
+                              width: `${Other}%`,
+                            }}
+                          />
+                        </div>
+                        <h1 className="metrics-page__bar-percentage-text">
+                          {Other}
+                          <span>%</span>
+                        </h1>
+                      </div>
+                    )
+                  }
 
                 </div>
               </div>
@@ -84,86 +139,155 @@ function MetricsPage() {
             <h1 className="metrics-page__title">Features</h1>
             <div className="metrics-page__features-box">
               <p className="metrics-page__subtitle">Main colors</p>
-              <div className="metrics-page__features-box-colors">
-                {colorsArray.map((color) => (
+              {isLoading ? (
+                <div style={{
+                  textAlign: `left`,
+                }}
+                >
+                  <span className="metrics-page__loader--default" />
                   <span
-                    key={color.blue + color.red + color.green}
-                    className="metrics-page__features-color"
-                    style={{
-                      backgroundColor: `rgb(${color.red},${color.green},${color.blue})`,
-                    }}
-                  />
-                ))}
-              </div>
+                    className="metrics-page__image-loader-text"
+                  >
+                    Almost done
+                  </span>
+                </div>
+              ) : (
+                <div className="metrics-page__features-box-colors">
+                  {colorsArray.map((color) => (
+                    <span
+                      key={color.blue + color.red + color.green}
+                      className="metrics-page__features-color"
+                      style={{
+                        backgroundColor: `rgb(${color.red},${color.green},${color.blue})`,
+                      }}
+                    />
+                  ))}
+                </div>
+              )}
 
             </div>
             <div className="metrics-page__features-box">
               <p className="metrics-page__subtitle">Emotions</p>
-              <div className="metrics-page__features-box-items">
-                {emotionsArray.map((emotion) => (
+              {isLoading ? (
+                <div style={{
+                  textAlign: `left`,
+                }}
+                >
+                  <span className="metrics-page__loader--default" />
                   <span
-                    key={emotion}
-                    className="metrics-page__features-item"
+                    className="metrics-page__image-loader-text"
                   >
-                    {emotion}
+                    It&apos;s going to take a little longer than we expected
                   </span>
-                ))}
-              </div>
+                </div>
+              ) : (
+                <div className="metrics-page__features-box-items">
+                  {emotionsArray.map((emotion) => (
+                    <span
+                      key={emotion}
+                      className="metrics-page__features-item"
+                    >
+                      {emotion}
+                    </span>
+                  ))}
+                </div>
+              )}
 
             </div>
             <div className="metrics-page__features-box">
               <p className="metrics-page__subtitle">Objects</p>
-              <div className="metrics-page__features-box-items">
-                {objectsArray.map((object) => (
+              {isLoading ? (
+                <div style={{
+                  textAlign: `left`,
+                }}
+                >
+                  <span className="metrics-page__loader--default" />
                   <span
-                    key={object}
-                    className="metrics-page__features-item"
+                    className="metrics-page__image-loader-text"
                   >
-                    {object}
+                    Select objects from the photo (usually takes about 3 minutes)
                   </span>
-                ))}
-              </div>
+                </div>
+              ) : (
+                <div className="metrics-page__features-box-items">
+                  {objectsArray.map((object) => (
+                    <span
+                      key={object}
+                      className="metrics-page__features-item"
+                    >
+                      {object}
+                    </span>
+                  ))}
+                </div>
+              )}
 
             </div>
             <div className="metrics-page__features-box">
               <p className="metrics-page__subtitle">Associations</p>
-              <div className="metrics-page__features-box-items">
-                {associationsArray.map((association) => (
+              {isLoading ? (
+                <div style={{
+                  textAlign: `left`,
+                }}
+                >
+                  <span className="metrics-page__loader--default" />
                   <span
-                    key={association}
-                    className="metrics-page__features-item"
+                    className="metrics-page__image-loader-text"
                   >
-                    {association}
+                    It&apos;s going to take a little longer than we expected
                   </span>
-                ))}
-              </div>
+                </div>
+              ) : (
+                <div className="metrics-page__features-box-items">
+                  {associationsArray.map((association) => (
+                    <span
+                      key={association}
+                      className="metrics-page__features-item"
+                    >
+                      {association}
+                    </span>
+                  ))}
+                </div>
+              )}
 
             </div>
           </div>
         </div>
         <div className="metrics-page__similar-photos">
           <h1 className="metrics-page__title">Similar photos</h1>
-          {
-            similarPhotosArray.length === 0 ? (
-              <div className="metrics-page__similar-photos-empty-container">
-                <p className="metrics-page__similar-photos-empty">
-                  The photo is unique so it has
-                  no similar photos
-                </p>
-              </div>
-            ) : (
-              <div className="metrics-page__similar-photos-full-container">
-                {similarPhotosArray.map((photo) => (
-                  <MetricsSimilarCard
-                    id={photo.photoId}
-                    image={photo.photoPath}
-                    relatedColors={photo.relatedColors}
-                    relatedFeatures={photo.relatedFeatures}
-                  />
-                ))}
-              </div>
-            )
-          }
+          {isLoading ? (
+            <div className="metrics-page__similar-photos-empty-container">
+              <img
+                src={MetricSimilarLoader}
+                alt="loader similar images"
+                draggable={false}
+                style={{
+                  marginBottom: `24px`,
+                }}
+              />
+              <p className="metrics-page__similar-photos-empty">
+                We&apos;ll start searching for photos once we&apos;ve highlighted
+                all the tags
+              </p>
+            </div>
+          ) : similarPhotosArray.length === 0 ? (
+            <div className="metrics-page__similar-photos-empty-container">
+              <p className="metrics-page__similar-photos-empty">
+                The photo is unique so it has
+                no similar photos
+              </p>
+            </div>
+          ) : (
+            <div className="metrics-page__similar-photos-full-container">
+              {similarPhotosArray.map((photo) => (
+                <MetricsSimilarCard
+                  id={photo.photoId}
+                  image={photo.photoPath}
+                  relatedColors={photo.relatedColors}
+                  relatedFeatures={photo.relatedFeatures}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
